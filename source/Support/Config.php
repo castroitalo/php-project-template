ccdc279df90d3e97180f8ce91910374ce91cd23e
Italo Castro <dev.castro.italo@gmail.com>
2023-06-02 12:09:07 -0300

    first commit

---
+++ source/Support/Config.php
@@ -0,0 +1,22 @@
+/**
+ * DATABASE
+ */
+define("CONF_DB_HOST", "");
+define("CONF_DB_USER", "");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "");
+
+/**
+ * DATE
+ */
+define("CONF_DATE_BR", "d/m/Y H:i:s");
+define("CONF_DATE_APP", "Y-m-d H:i:s");
+
+/**
+ * MESSAGE
+ */
+define("CONF_MESSAGE_CLASS", "trigger");
+define("CONF_MESSAGE_INFO", "info");
+define("CONF_MESSAGE_SUCCESS", "success");
+define("CONF_MESSAGE_WARNING", "warning");
+define("CONF_MESSAGE_ERROR", "error");
