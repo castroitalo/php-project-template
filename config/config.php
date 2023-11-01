a0880256ae6567a05beda99bc772f7f8d32905d1
devcastroitalo <dev.castro.italo@gmail.com>
2023-11-01 11:09:44 -0300

    flash and session class added

--- config/config.php
+++ config/config.php
@@ -4,3 +4,4 @@
 require_once __DIR__ . "/config-files/general.config.php";
 require_once __DIR__ . "/config-files/routes.config.php";
 require_once __DIR__ . "/config-files/view.config.php";
+require_once __DIR__ . "/config-files/flash.config.php";
